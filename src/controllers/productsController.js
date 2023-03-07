e35d754e641045c7247f2d262835e0384a378932
Maxgg21 <lionel.gegena@gmail.com>
2023-03-07 01:20:11 -0300

    Agregando multer a los formularios, rutas y controladores

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -49,7 +49,7 @@ const controller = {
 			discount: req.body.discount,
 			category: req.body.category,
 			description: req.body.description,
-			image: req.file ? req.file.name : 'default-image.png'
+			image: req.file ? req.file.filename : 'default-image.png'
 		}
 		products.push(newProduct);
 
@@ -80,7 +80,7 @@ const controller = {
 			product.discount = req.body.discount,
 			product.category = req.body.category,
 			product.description = req.body.description
-			product.image = req.file.filename
+			product.image = req.file ? req.file.filename : product.image;
 			}
 		});
 
